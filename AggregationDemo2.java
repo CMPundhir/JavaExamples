c4180f4560294992a2123c5d77152e831f8c2ada
CMPundhir <g.pundhir8802805015@gmail.com>
2019-06-17 11:04:02 +0530

    Aggregation demo 2 updated

--- AggregationDemo2.java
+++ AggregationDemo2.java
@@ -1,19 +1,37 @@
-{
-	"vendorId":"1",
-	"userId":"1",
-	"brands":
-		[
-			{
-				"brandId":"",
-				"productId":""
-			},
-			{
-				
-			},
-			{
-			
-			},
-			{
-			
-			}
-		]
+class Address{
+	int hNo;
+	String street;
+	int pincode;
+	String city;
+	public Address(int hNo,String street,int pincode,String city){
+		this.hNo = hNo;
+		this.street = street;
+		this.pincode = pincode;
+		// this.city = city;
+	}
+	@Override
+	public String toString(){
+		return "Address{\n house no : "+hNo+",\n street : "+street+",\n pincode : "+pincode+",\n city : "+city+"}";
+	}
+}
+class Student{
+	int rn;
+	String name;
+	Address addr;
+	public Student(int rn,String name,Address addr){
+		this.rn = rn;
+		this.name = name;
+		this.addr = addr;
+	}
+	@Override
+	public String toString(){
+		return "Student{\n roll no :"+rn+",\n name : "+name+",\n address : "+addr+"}";
+	}
+}
+class AggregationDemo2{
+	public static void main(String[] args){
+		Address addr = new Address(108,"A- Block",110033,"Delhi");
+		Student s = new Student(1,"Tanishq",addr);
+		System.out.println(s);
+	}
+}
