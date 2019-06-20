3804ce033affcf7704bc8f5018bdfd7827ba2301
CMPundhir <g.pundhir8802805015@gmail.com>
2019-06-20 17:08:11 +0530

    Input demo using scanner

---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Input{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		int a = sc.nextInt();
+	}
+}
