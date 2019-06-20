30419165f202ee7aef533a586cc66bf129a5d0b5
CMPundhir <g.pundhir8802805015@gmail.com>
2019-06-20 11:41:42 +0530

    Multiple inheritance with annonymous object

--- MultipleInheritance.java
+++ MultipleInheritance.java
@@ -15,10 +15,26 @@ class MultipleInheritance{
 		c.method();
 		
 		A a = new A(){
+			@Override
 			public void method(){
-				System.out.println("HAHhahahahahhaha");
+				System.out.println("Hello Method");
 			}
 		};
 		a.method();
+		
+		//annonymous object
+		
+		new C().method();
+		
+		new A(){
+			@Override
+			public void method(){
+				System.out.println("I am annonymously created and overriden");
+			}
+		}.method();
 	}
 }
+
+
+
+
